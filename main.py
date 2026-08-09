a7f2288f3389f69e4fdfd9f9cd4049421937a6ba
jawmullins <ayymullins@gmail.com>
2026-08-09 12:20:18 +1200

    price hist plot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 def read_data(data_path="data"):
     """
     reads the 9 listings.csv files from the /data folder then
@@ -23,13 +22,29 @@ def read_data(data_path="data"):
     # combines the list of datasets into one pandas data frame
     return pd.concat(data_files)
 
+# price histogram
+
+# remove missing price values??
+
+def plot_hist(values):
+    """plot the prices in a histogram"""
+    price = values['price']
+    axes = plt.axes()
+    axes.hist(price, bins=np.linspace(0,1500,15), edgecolor='steelblue', color='skyblue')
+    axes.grid(True)
+    axes.set_title("Price density of AirBnBs in Christchurch")
+    axes.set_xlabel('Nightly price ($)')
+    axes.set_ylabel('Count')
+    plt.show()
 
 def main():
     data = read_data()
     print(data[["number_of_reviews", "price"]].head())
     print(data[["number_of_reviews", "price"]].describe())
     print(data["price"].isna().sum())
-
+    # filter data by chch location only <-- alex temp filtering for plots
+    chch_data = data[data['neighbourhood_group'] == 'Christchurch City']
+    plot_hist(chch_data)  # using max price of $1500
 
 if __name__ == "__main__":
     main()
